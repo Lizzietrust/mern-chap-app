c45621cb6a5e6adc57db19e96f23d103280c7a82
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-09-03 03:11:18 +0100

    Add user info and profile update routes to AuthRoutes

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { register, login, logout } from "../controllers/AuthController.js";
+import { register, login, logout, getUserInfo, updateProfile } from "../controllers/AuthController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/register", register);
 authRoutes.post("/login", login);
 authRoutes.post("/logout", logout);
+authRoutes.get("/user-info", verifyToken, getUserInfo);
+authRoutes.put("/update-profile", verifyToken, updateProfile);
 
 export default authRoutes;
