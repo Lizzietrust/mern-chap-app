67ff3090202c5f254ab8036cb9f0a3a94e893d83
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-09-08 01:17:52 +0100

    "feat: Implement message sending and receiving through sockets"

--- server/socket.js
+++ server/socket.js
@@ -1,4 +1,5 @@
 import { Server } from "socket.io";
+import Message from "./models/MessageModel.js";
 
 const setupSocket = (server) => {
   const io = new Server(server, {
@@ -9,6 +10,25 @@ const setupSocket = (server) => {
   });
 
   const userSockets = new Map();
+  
+  const sendMessage = async(message) => {
+    const senderSocketId = userSockets.get(message.sender);
+    const recipientSocketId = userSockets.get(message.recipient);
+    const createdMessage = await Message.create(message);
+    const messageData = await Message.findById(createMessage._id).populate(
+      "sender", "_id firstName lastName image email bio phone location website color profileSetup"
+    ).populate(
+      "recipient", "_id firstName lastName image email bio phone location website color profileSetup"
+    );
+
+    if (senderSocketId) {
+      io.to(senderSocketId).emit("messageSent", messageData);
+    }
+    if (recipientSocketId) {
+      io.to(recipientSocketId).emit("messageReceived", messageData);
+    }
+  }
+
 
   io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
@@ -18,6 +38,8 @@ const setupSocket = (server) => {
     } else {
       console.log("User connected without userId");
     }
+
+    socket.on("sendMessage", sendMessage) 
 
     socket.on("disconnect", () => {
       const disconnectedUserId = [...userSockets.entries()].find(
