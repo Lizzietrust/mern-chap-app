d520640e7e633029b0cf3ede16fd588627de55de
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-09-12 14:28:40 +0100

    `feat: Add sendMessage and getMessages controllers for messages in chat`

--- server/controllers/MessageController.js
+++ server/controllers/MessageController.js
@@ -1,4 +1,5 @@
 import Chat from "../models/ChatModel.js";
+import Message from "../models/MessageModel.js";
 
 export const createChat = async (req, res, next) => {
   try {
@@ -18,6 +19,63 @@ export const createChat = async (req, res, next) => {
     const savedChat = await newChat.save();
 
     res.status(201).json(savedChat);
+  } catch (error) {
+    console.log({ error });
+    next(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const sendMessage = async (req, res, next) => {
+  try {
+    const { chatId, content } = req.body;
+    const sender = req.userId; // Assuming req.userId is set by AuthMiddleware
+
+    const newMessage = new Message({
+      sender,
+      message: "text", // Assuming text messages for now
+      content,
+    });
+
+    const savedMessage = await newMessage.save();
+
+    await Chat.findByIdAndUpdate(
+      chatId,
+      { $push: { messages: savedMessage._id } },
+      { new: true }
+    );
+
+    res.status(201).json(savedMessage);
+  } catch (error) {
+    console.log({ error });
+    next(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res, next) => {
+  try {
+    const { chatId } = req.params;
+
+    const chat = await Chat.findById(chatId).populate("messages");
+
+    if (!chat) {
+      return res.status(404).json({ message: "Chat not found" });
+    }
+
+    res.status(200).json(chat.messages);
+  } catch (error) {
+    console.log({ error });
+    next(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getUserChats = async (req, res, next) => {
+  try {
+    const userId = req.userId; // Assuming req.userId is set by AuthMiddleware
+    const chats = await Chat.find({ participants: userId }).populate('participants'); // Populate participants to get user details
+    res.status(200).json(chats);
   } catch (error) {
     console.log({ error });
     next(error);
