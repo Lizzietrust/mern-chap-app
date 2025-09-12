296707fc41636d66637bb71229274f33e0f4f5b4
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-09-12 14:29:24 +0100

    fp: Add sendMessage, getMessages, and getUserChats functions to MessageRoutes.js

--- server/routes/MessageRoutes.js
+++ server/routes/MessageRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createChat } from "../controllers/MessageController.js";
+import { createChat, sendMessage, getMessages, getUserChats } from "../controllers/MessageController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const messageRoutes = express.Router();
 
 messageRoutes.post("/create-chat", verifyToken, createChat);
+messageRoutes.post("/send-message", verifyToken, sendMessage);
+messageRoutes.get("/get-messages/:chatId", verifyToken, getMessages);
+messageRoutes.get("/get-user-chats", verifyToken, getUserChats);
 
 export default messageRoutes;
