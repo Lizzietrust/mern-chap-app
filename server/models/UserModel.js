b9720a46bee288ce08252f17a1cdcdb17920041a
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-09-19 21:06:26 +0100

    fix: correct model name from "Users" to "User"

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -58,6 +58,6 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
-const User = mongoose.model("Users", userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
