efc82e4b95746ac6ba03c99afb1e26df2a370dbb
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-12 17:17:16 +0100

    feat: Add profile fields to UserMode

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,52 +1,55 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    firstName: {
+      type: String,
+      required: false,
+    },
+    lastName: {
+      type: String,
+      required: false,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    bio: {
+      type: String,
+      required: false,
+      maxlength: 500,
+    },
+    phone: {
+      type: String,
+      required: false,
+    },
+    location: {
+      type: String,
+      required: false,
+    },
+    website: {
+      type: String,
+      required: false,
+    },
+    profileSetup: {
+      type: Boolean,
+      default: false,
+    },
   },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-  firstName: {
-    type: String,
-    required: false,
-  },
-  lastName: {
-    type: String,
-    required: false,
-  },
-  image: {
-    type: String,
-    required: false,
-  },
-  bio: {
-    type: String,
-    required: false,
-    maxlength: 500,
-  },
-  phone: {
-    type: String,
-    required: false,
-  },
-  location: {
-    type: String,
-    required: false,
-  },
-  website: {
-    type: String,
-    required: false,
-  },
-  profileSetup: {
-    type: Boolean,
-    default: false,
-  },
-}, {
-  timestamps: true, // Add createdAt and updatedAt fields
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
