698a4486beaa934ae21ae034859dc739454913a2
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-17 04:06:27 +0100

    feat: add new chat modal types

---
+++ client/src/types/NewChatModal.types.ts
@@ -0,0 +1,46 @@
+import type { User } from "./types";
+
+export interface NewChatModalProps {
+  isDark: boolean;
+  onClose: () => void;
+  users: User[];
+  handleSelectUser: (userId: string) => void;
+  currentPage: number;
+  totalUsers: number;
+  onPageChange: (page: number) => void;
+  onSearch: (searchTerm: string) => void;
+  isLoading?: boolean;
+  searchTerm: string;
+  onSearchTermChange: (searchTerm: string) => void;
+}
+
+export interface SearchInputProps {
+  searchTerm: string;
+  onSearchChange: (value: string) => void;
+  onClearSearch: () => void;
+  isDark: boolean;
+  placeholder?: string;
+}
+
+export interface UserListProps {
+  users: User[];
+  isLoading: boolean;
+  searchTerm: string;
+  onUserSelect: (userId: string) => void;
+  isDark: boolean;
+}
+
+export interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onNextPage: () => void;
+  onPrevPage: () => void;
+  isLoading: boolean;
+  isDark: boolean;
+}
+
+export interface UserItemProps {
+  user: User;
+  onSelect: (userId: string) => void;
+  isDark: boolean;
+}
