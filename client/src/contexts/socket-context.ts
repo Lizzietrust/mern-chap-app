ed388ed5035194e90709e10ecf4bf0c670edc511
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-18 04:16:01 +0100

    fix: remove unused socket context

--- client/src/contexts/socket-context.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import type { SocketContextType } from "./SocketContext";
-
-export const SocketContext = createContext<SocketContextType | undefined>(
-  undefined
-);
