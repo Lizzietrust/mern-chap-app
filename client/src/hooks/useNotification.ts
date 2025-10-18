f7636cd65b261562ee461af35f15636526434929
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-18 02:52:50 +0100

    feat: add useNotifications hook

---
+++ client/src/hooks/useNotification.ts
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { NotificationContext } from "../contexts/NotificationContext";
+import type { NotificationContextType } from "../types/notification";
+
+export function useNotifications(): NotificationContextType {
+  const context = useContext(NotificationContext);
+
+  if (context === undefined) {
+    throw new Error(
+      "useNotifications must be used within a NotificationProvider"
+    );
+  }
+
+  return context;
+}
