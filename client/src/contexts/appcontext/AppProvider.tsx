a4e5326b94416809450e9243408e1416ac6aad95
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-18 02:20:59 +0100

    feat: add app provider context

---
+++ client/src/contexts/appcontext/AppProvider.tsx
@@ -0,0 +1,35 @@
+import { useReducer } from "react";
+import type { ReactNode } from "react";
+import { SelectedChatProvider } from "../SelectedChatContext";
+import { AppLogic } from "../AppLogic";
+import { appReducer, initialState } from "../reducers/appReducer";
+import { AppContext } from "./appConstants"; 
+// import type { AppState, AppAction } from "../../types/app";
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+export function AppProvider({ children }: AppProviderProps) {
+  const [state, dispatch] = useReducer(appReducer, initialState);
+
+  const placeholderActions = {
+    login: () => {},
+    logout: () => {},
+    toggleTheme: () => {},
+    toggleSidebar: () => {},
+    addNotification: () => {},
+    removeNotification: () => {},
+    setLoading: () => {},
+  };
+
+  return (
+    <AppContext.Provider value={{ state, dispatch, ...placeholderActions }}>
+      <SelectedChatProvider>
+        <AppLogic state={state} dispatch={dispatch}>
+          {children}
+        </AppLogic>
+      </SelectedChatProvider>
+    </AppContext.Provider>
+  );
+}
