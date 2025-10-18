f5bbeb3994a684ad664b1f79df411983c0c976fc
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-18 02:18:38 +0100

    fix: update import path for useApp

--- client/src/components/chat/create-channel-modal.tsx/useCreateChannelModal.ts
+++ client/src/components/chat/create-channel-modal.tsx/useCreateChannelModal.ts
@@ -2,7 +2,7 @@ import { useState, useCallback } from "react";
 import { useUsers } from "../../../hooks/useUsers";
 import { useCreateChannel } from "../../../hooks/useChannels";
 import { useNotifications } from "../../../contexts/NotificationContext";
-import { useApp } from "../../../contexts/AppContext";
+import { useApp } from "../../../contexts/appcontext/index";
 import type { ChannelFormData } from "../../../types/CreateChannelModal.types";
 import type { ChannelChat } from "../../../types/types";
 
