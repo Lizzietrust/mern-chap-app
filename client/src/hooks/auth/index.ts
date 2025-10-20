a9f844f03f3d7332fcfec425a5b72ec89ceaea46
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-20 03:44:31 +0100

    feat: add auth hooks exports

---
+++ client/src/hooks/auth/index.ts
@@ -0,0 +1,14 @@
+export {
+  useAuth,
+  useAuthState,
+  useAuthActions,
+  useCurrentUser,
+} from "../useAuth";
+export {
+  useLogin,
+  useRegister,
+  useLogout,
+  useUpdateProfile,
+  useMe,
+} from "../useAuthLogic";
+export { AUTH_KEYS } from "../../constants/auth";
