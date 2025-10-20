e1f411fdc4697fb8d01fa83891993008a8689d30
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-20 15:26:45 +0100

    feat: export chat subtitle config and user status

--- client/src/hooks/chats/index.ts
+++ client/src/hooks/chats/index.ts
@@ -7,3 +7,5 @@ export {
 } from "./mutations";
 export { CHAT_KEYS, MESSAGE_KEYS } from "../../constants/chat";
 export type { SendMessageData, ChatQueryOptions } from "../../types/chat";
+export type { ChatSubtitleConfig, UserStatus } from "../../types/chat";
+export { DEFAULT_SUBTITLE_CONFIG, STATUS_TEXTS } from "../../constants/chat";
