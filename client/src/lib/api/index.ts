a913c0d1c021cc1226abb3578c882e72cfe0bf9d
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-21 14:42:47 +0100

    feat: add api exports

---
+++ client/src/lib/api/index.ts
@@ -0,0 +1,12 @@
+export { axiosInstance } from "./axiosInstance";
+export { apiClient } from "./ApiClient";
+export { API_BASE_URL } from "./axiosInstance";
+
+export { authApi } from "./services/authApi";
+export { userApi } from "./services/userApi";
+export { chatApi } from "./services/chatApi";
+export { channelApi } from "./services/channelApi";
+export { postApi } from "./services/postApi";
+
+export type { Post } from "./services/postApi";
+export type { CreateChatRequest } from "./services/chatApi";
