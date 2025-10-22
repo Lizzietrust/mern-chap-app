0c0312be8d6e13a909c23404047e8c2ba3bcfb8c
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-22 04:22:39 +0100

    feat: export chat hooks

--- client/src/hooks/chats/index.ts
+++ client/src/hooks/chats/index.ts
@@ -9,3 +9,7 @@ export { CHAT_KEYS, MESSAGE_KEYS } from "../../constants/chat";
 export type { SendMessageData, ChatQueryOptions } from "../../types/chat";
 export type { ChatSubtitleConfig, UserStatus } from "../../types/chat";
 export { DEFAULT_SUBTITLE_CONFIG, STATUS_TEXTS } from "../../constants/chat";
+export { useChatLogic } from "./useChatLogic";
+export { useChatData } from "./useChatData";
+export { useMessageHandling } from "./useMessageHandling";
+export { useSocketHandlers } from "./useSocketHandlers";
