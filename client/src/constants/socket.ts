02a4830462aa8e03db08afe511fd8b1e5f749c07
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-10-29 03:36:51 +0100

    feat: add new socket events for message status tracking

--- client/src/constants/socket.ts
+++ client/src/constants/socket.ts
@@ -13,6 +13,12 @@ export const SOCKET_EVENTS = {
   LEAVE_CHAT: "leaveChat",
   UPDATE_USER_STATUS: "updateUserStatus",
   MESSAGE_STATUS_UPDATE: "messageStatusUpdate",
+  GET_ONLINE_USERS: "getOnlineUsers",
+  MARK_AS_DELIVERED: "markAsDelivered",
+  MESSAGE_DELIVERED: "messageDelivered",
+  MESSAGE_READ: "messageRead",
+  MARK_MESSAGES_AS_READ: "markMessagesAsRead",
+  MARK_ALL_MESSAGES_AS_READ: "markAllMessagesAsRead",
 } as const;
 
 export const SOCKET_CONFIG = {
