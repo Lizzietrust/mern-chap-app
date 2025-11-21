3a1e03084b12ac3f0fb3afaa514c8bc7fd94afb1
Lizzietrust <ohidaelizabeth3@gmail.com>
2025-11-21 21:25:06 +0100

    feat: add user profile route

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { fetchAllUsers } from "../controllers/UserController.js";
+import { fetchAllUsers, getUserProfile } from "../controllers/UserController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const userRoutes = express.Router();
 
 userRoutes.get("/fetch-all-users", verifyToken, fetchAllUsers);
+userRoutes.get("/:userId/profile", getUserProfile);
 
 export default userRoutes;
